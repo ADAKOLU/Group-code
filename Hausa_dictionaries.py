26fcaa7f2f3504051ea650e2c2759689894ca93d
mowasola <mowasolaogunleye@gmail.com>
2025-12-17 15:00:18 +0100

    Update Hausa_ yoruba_dictionaries.py

--- Hausa_dictionaries.py
+++ Hausa_dictionaries.py
@@ -20,11 +20,41 @@ hausa = {
         "sleep": "barci"
         "eat": "ci"
 }
+yoruba = {
+    "man": "ọkùnrin",
+    "woman": "obinrin",
+    "sun": "oorun",
+    "moon": "osupa",
+    "money": "owo",
+    "road": "opopona",
+    "friend": "ọrẹ",
+    "love": "ifẹ",
+    "work": "iṣẹ",
+    "sleep": "sun",
+    "eat": "jẹ"
+    "woman": "obinrin",
+    "child": "ọmọ",
+    "father": "baba",
+    "mother": "iya",
+    "friend": "ọrẹ",
+    "teacher": "olukọ",
+    "star": "irawọ",
+    "water": "omi",
+    "fire": "ina",
+    "rain": "ojo",
+    "love": "ifẹ",
+    "happy": "ayọ",
+    "sad": "ibanujẹ",
+    "angry": "ibinu",   
+}
 translation ={
     'hausa':hausa,
+    'youruba': yoruba
 }
 language = input("enter what language; (hausa): ")
 word = input("enter the english word you want to translate: ")
+language = input("Enter language (hausa / yoruba): ")
+word = input("Enter the English word to translate: ")
 if language in translation:
     lang = translation[language]
     if word in lang:
